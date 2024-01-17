1a4644a3fe34e83eb89514c2f85ac941a9cee701
FARHAIN6427 <2020996427@student.uitm.edu.my>
2024-01-18 00:11:24 +0800

    First commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,36 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight ">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    HELLO!<b> {{ Auth::user()->name }}.</b>
+                    You are {{ Auth::user()->role}}
+                </div>
+            </div>
+
+            @if (Auth::user()->role =='admin')
+
+
+
+            <div class="mt-4 max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        ONLY ADMIN CAN SEE THIS!
+                    </div>
+                </div>
+                @endif
+        </div>
+
+
+    <div inline-datepicker data-date="02/25/2022"></div>
+
+    </div>
+</x-app-layout>
